2dc37dbb5362e8d11476512491cf1cd42f100b8b
marinakrivonosova <mary.krivonosova1@gmail.com>
2025-01-11 22:31:56 +0100

    first commit

---
+++ src/main/kotlin/easyLeetCodeTasks/LeetCodeEasy.kt
@@ -0,0 +1,4 @@
+package easyLeetCodeTasks
+
+class LeetCodeEasy {
+}
---
+++ src/test/kotlin/easyLeetCodeTasks/LeetCodeEasyTest.kt
@@ -0,0 +1,12 @@
+package easyLeetCodeTasks
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class LeetCodeEasyTest {
+
+    @Test
+    fun twoSum() {
+    }
+}
