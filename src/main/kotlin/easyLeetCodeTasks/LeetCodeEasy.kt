1288d465d4c676887cbff1e7f9d8eeac295b4930
marinakrivonosova <mary.krivonosova1@gmail.com>
2025-01-14 18:27:33 +0100

    leet code easy tasks solution

--- src/main/kotlin/easyLeetCodeTasks/LeetCodeEasy.kt
+++ src/main/kotlin/easyLeetCodeTasks/LeetCodeEasy.kt
@@ -1,4 +1,265 @@
 package easyLeetCodeTasks
 
 class LeetCodeEasy {
+
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val result = mutableMapOf<Int, Int>()
+        for (i in nums.indices) {
+            val diff = target - nums[i]
+            if (result.containsKey(diff)) {
+                return intArrayOf(result.getValue(diff), i)
+            } else {
+                result[nums[i]] = i
+            }
+        }
+        return intArrayOf(0, 0)
+    }
+
+    fun isPalindrome(x: Int): Boolean {
+        val stringVer = x.toString()
+        return if (stringVer[0] != stringVer[stringVer.length - 1]) false
+        else {
+            var isPal = true
+            for (i in 0..stringVer.length / 2) {
+                if (stringVer[i] != stringVer[stringVer.length - 1 - i])
+                    isPal = false
+            }
+            return isPal
+        }
+    }
+
+    fun isPalindromeHalfNumberConverting(x: Int): Boolean {
+        var xCopy = x
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false
+        var reversed = 0
+
+        while (reversed < xCopy) {
+            reversed = (reversed * 10) + (xCopy % 10)
+            xCopy /= 10
+        }
+
+        return xCopy == reversed || xCopy == reversed / 10
+    }
+
+    fun isPalindromeReverseString(x: Int): Boolean {
+        val stringVer = x.toString()
+        val reversed = stringVer.reversed()
+        return stringVer == reversed
+    }
+
+    fun romanToIntUsingSwitch(s: String): Int {
+        var result = 0
+
+        for (i in 0..s.length - 2) {
+            result += when (s[i]) {
+                'I' -> if (s[i + 1] == 'V' || s[i + 1] == 'X') -1 else 1
+                'V' -> 5
+                'X' -> if (s[i + 1] == 'L' || s[i + 1] == 'C') -10 else 10
+                'L' -> 50
+                'C' -> if (s[i + 1] == 'D' || s[i + 1] == 'M') -100 else 100
+                'D' -> 500
+                'M' -> 1000
+                else -> throw IllegalArgumentException("Unexpected char '${s[i]}'")
+            }
+        }
+
+        when (s[s.length - 1]) {
+            'I' -> result += 1
+            'V' -> result += 5
+            'X' -> result += 10
+            'L' -> result += 50
+            'C' -> result += 100
+            'D' -> result += 500
+            'M' -> result += 1000
+        }
+        return result
+    }
+
+    fun romanToInt(s: String): Int {
+        var result = 0
+        val dictionary = mapOf(
+            'I' to 1,
+            'V' to 5,
+            'X' to 10,
+            'L' to 50,
+            'C' to 100,
+            'D' to 500,
+            'M' to 1000
+        )
+
+        for (i in 0 until s.lastIndex) {
+            if ((dictionary[s[i]] ?: 0) < (dictionary[s[i + 1]] ?: 0)) result -= dictionary[s[i]] ?: 0
+            else result += dictionary[s[i]] ?: throw IllegalArgumentException("Unexpected char '${s[i]}'")
+
+        }
+        return result + (dictionary[s.last()] ?: 0)
+    }
+
+    fun longestCommonPrefix(strs: Array<String>): String {
+        if (strs.isEmpty()) return ""
+        val firstWord = strs[0]
+        for (i in firstWord.indices) {
+            for (s in strs) {
+                if (s.length == i || s[i] != firstWord[i]) return firstWord.substring(0, i)
+            }
+        }
+        return firstWord
+    }
+
+    fun isValidParentheses(s: String): Boolean {
+        val stack = ArrayDeque<Char>()
+        val pair = mapOf(
+            '(' to ')',
+            '{' to '}',
+            '[' to ']'
+        )
+        for (el in s) {
+            if (pair.containsKey(el)) {
+                stack.addLast(el)
+            } else if (stack.isEmpty()
+                || el != pair.getOrDefault(stack.removeLast(), "")
+            )
+                return false
+        }
+        return stack.isEmpty()
+    }
+
+    data class ListNode(var `val`: Int, var next: ListNode? = null)
+
+    fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
+        if (list1 == null) return list2
+        if (list2 == null) return list1
+
+        return if (list1.`val` < list2.`val`) {
+            ListNode(list1.`val`, mergeTwoLists(list1.next, list2))
+        } else {
+            ListNode(list2.`val`, mergeTwoLists(list2.next, list1))
+        }
+    }
+
+    fun mergeTwoListsNoRecursion(list1: ListNode?, list2: ListNode?): ListNode? {
+        var p1 = list1
+        var p2 = list2
+        var result: ListNode? = null
+        var lastElement: ListNode? = null
+
+        fun append(node: ListNode?) {
+            if (lastElement == null) {
+                lastElement = node
+                result = lastElement
+            } else {
+                lastElement!!.next = node
+                lastElement = lastElement!!.next
+            }
+        }
+
+        while (p1 != null && p2 != null) {
+            if (p1.`val` < p2.`val`) {
+                append(ListNode(p1.`val`))
+                p1 = p1.next
+            } else {
+                append(ListNode(p2.`val`))
+                p2 = p2.next
+            }
+        }
+
+        append(p1 ?: p2)
+
+        return result
+    }
+
+    fun removeDuplicates(nums: IntArray): Int {
+        if (nums.isEmpty()) return 0
+        var k = 1
+        for (i in 1..nums.lastIndex) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k] = nums[i]
+                k++
+            }
+        }
+
+        return k
+    }
+
+    fun removeElement(nums: IntArray, `val`: Int): Int {
+        if (nums.isEmpty()) return 0
+        var k = 0
+        for (x in nums) {
+            if (x != `val`) {
+                nums[k] = x
+                k++
+            }
+        }
+        return k
+    }
+
+
+    //28. Find the Index of the First Occurrence in a String
+    fun strStr(haystack: String, needle: String): Int {
+        return haystack.indexOf(needle)
+    }
+
+    //28. Find the Index of the First Occurrence in a String
+    fun strStrWithoutBuildInFunction(haystack: String, needle: String): Int {
+        val needleLength = needle.length
+        val haystackLength = haystack.length
+        if (haystackLength < needleLength) return -1
+        if (needle.isEmpty() || haystack == needle) return 0
+
+        val end = haystackLength - needleLength
+
+        for (i in 0..end) {
+            if (haystack.substring(i, needleLength + i) == needle) {
+                return i
+            }
+        }
+        return -1
+    }
+
+    // 35. Search Insert Position
+    fun searchInsert(nums: IntArray, target: Int): Int {
+        var start = 0
+        var end = nums.size - 1
+
+        while (start <= end) {
+            val middle = (end + start) / 2
+            if (nums[middle] > target)
+                end = middle - 1
+            else if (nums[middle] < target)
+                start = middle + 1
+            else return middle
+        }
+        return start
+    }
+
+    fun binarySearch(nums: IntArray, target: Int): Int {
+        var start = 0
+        var end = nums.lastIndex
+
+        while (start <= end) {
+            val middle = (end + start) / 2
+            if (nums[middle] == target) {
+                return middle
+            } else if (nums[middle] < target) {
+                start = middle + 1
+            } else end = middle - 1
+        }
+        return -1
+    }
+
+    fun binarySearchRecursion(nums: IntArray, target: Int, start: Int, end: Int): Int {
+        if (end >= start) {
+            val middle = (end + start) / 2
+            if (nums[middle] == target) {
+                return middle
+            } else if (nums[middle] < target) {
+                return binarySearchRecursion(nums, target, middle + 1, end)
+            } else return binarySearchRecursion(nums, target, start, middle - 1)
+        }
+        return -1
+    }
+
+    fun lengthOfLastWord(s: String): Int {
+        return s.split(' ').last { it.isNotEmpty() }.length
+    }
 }
--- src/test/kotlin/easyLeetCodeTasks/LeetCodeEasyTest.kt
+++ src/test/kotlin/easyLeetCodeTasks/LeetCodeEasyTest.kt
@@ -1,12 +1,194 @@
 package easyLeetCodeTasks
 
-import org.junit.jupiter.api.Test
-
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
 
 internal class LeetCodeEasyTest {
 
+    private val leetCodeEasy = LeetCodeEasy()
+
     @Test
     fun twoSum() {
+        assertArrayEquals(intArrayOf(0, 1), leetCodeEasy.twoSum(intArrayOf(2, 7, 11, 15), 9))
+        assertArrayEquals(intArrayOf(1, 2), leetCodeEasy.twoSum(intArrayOf(3, 2, 4), 6))
+        assertArrayEquals(intArrayOf(0, 1), leetCodeEasy.twoSum(intArrayOf(3, 3), 6))
+    }
+
+    @Test
+    fun isPalindrome() {
+        assertEquals(true, leetCodeEasy.isPalindrome(121))
+        assertEquals(false, leetCodeEasy.isPalindrome(-121))
+        assertEquals(false, leetCodeEasy.isPalindrome(10))
+        assertEquals(true, leetCodeEasy.isPalindrome(1234321))
+        assertEquals(true, leetCodeEasy.isPalindrome(0))
+
+        assertEquals(true, leetCodeEasy.isPalindromeReverseString(121))
+        assertEquals(false, leetCodeEasy.isPalindromeReverseString(-121))
+        assertEquals(false, leetCodeEasy.isPalindromeReverseString(10))
+        assertEquals(true, leetCodeEasy.isPalindromeReverseString(1234321))
+        assertEquals(true, leetCodeEasy.isPalindromeReverseString(0))
+
+        assertEquals(true, leetCodeEasy.isPalindromeHalfNumberConverting(121))
+        assertEquals(false, leetCodeEasy.isPalindromeHalfNumberConverting(-121))
+        assertEquals(false, leetCodeEasy.isPalindromeHalfNumberConverting(10))
+        assertEquals(true, leetCodeEasy.isPalindromeHalfNumberConverting(1234321))
+        assertEquals(true, leetCodeEasy.isPalindromeHalfNumberConverting(0))
+    }
+
+    @Test
+    fun romanToInt() {
+        assertEquals(3, leetCodeEasy.romanToIntUsingSwitch("III"))
+        assertEquals(58, leetCodeEasy.romanToIntUsingSwitch("LVIII"))
+        assertEquals(1994, leetCodeEasy.romanToIntUsingSwitch("MCMXCIV"))
+
+        assertEquals(3, leetCodeEasy.romanToInt("III"))
+        assertEquals(58, leetCodeEasy.romanToInt("LVIII"))
+        assertEquals(1994, leetCodeEasy.romanToInt("MCMXCIV"))
+    }
+
+    @Test
+    fun longestCommonPrefix() {
+        assertEquals("a", leetCodeEasy.longestCommonPrefix(arrayOf("a")))
+        assertEquals("", leetCodeEasy.longestCommonPrefix(arrayOf("", "")))
+        assertEquals("", leetCodeEasy.longestCommonPrefix(arrayOf("", "a")))
+        assertEquals("", leetCodeEasy.longestCommonPrefix(arrayOf("a", "")))
+        assertEquals("a", leetCodeEasy.longestCommonPrefix(arrayOf("ab", "a")))
+        assertEquals("", leetCodeEasy.longestCommonPrefix(arrayOf("a", "b")))
+        assertEquals("", leetCodeEasy.longestCommonPrefix(arrayOf("")))
+        assertEquals("fl", leetCodeEasy.longestCommonPrefix(arrayOf("flower", "flow", "flight")))
+        assertEquals("", leetCodeEasy.longestCommonPrefix(arrayOf("dog", "racecar", "car")))
+
+    }
+
+    @Test
+    fun isValidParentheses() {
+        assertTrue(leetCodeEasy.isValidParentheses("[]"))
+        assertTrue(leetCodeEasy.isValidParentheses("[{}]"))
+        assertTrue(leetCodeEasy.isValidParentheses("[(){}]"))
+        assertTrue(leetCodeEasy.isValidParentheses("[{()}]"))
+        assertFalse(leetCodeEasy.isValidParentheses("[{()}}"))
+        assertFalse(leetCodeEasy.isValidParentheses("[}}"))
+        assertFalse(leetCodeEasy.isValidParentheses("[]}"))
+    }
+
+    @Test
+    fun mergeTwoListWithRecursion() {
+        assertEquals(
+            LeetCodeEasy.ListNode(1, LeetCodeEasy.ListNode(2, LeetCodeEasy.ListNode(3))),
+            leetCodeEasy.mergeTwoLists(
+                LeetCodeEasy.ListNode(1),
+                LeetCodeEasy.ListNode(2, LeetCodeEasy.ListNode(3))
+            )
+        )
+    }
+
+    @Test
+    fun mergeTwoList() {
+        assertEquals(
+            LeetCodeEasy.ListNode(1, LeetCodeEasy.ListNode(2, LeetCodeEasy.ListNode(3))),
+            leetCodeEasy.mergeTwoListsNoRecursion(
+                LeetCodeEasy.ListNode(1),
+                LeetCodeEasy.ListNode(2, LeetCodeEasy.ListNode(3))
+            )
+        )
+    }
+
+    @Test
+    fun removeDuplicates() {
+        assertEquals(2, leetCodeEasy.removeDuplicates(intArrayOf(1, 2, 2)))
+        assertEquals(5, leetCodeEasy.removeDuplicates(intArrayOf(0, 0, 1, 1, 1, 2, 2, 3, 3, 4)))
+    }
+
+    @Test
+    fun `test find the index of the first occurrence in a string`() {
+        assertEquals(0, leetCodeEasy.strStr("sadbutsad", "sad"))
+        assertEquals(3, leetCodeEasy.strStr("sadbutsad", "but"))
+        assertEquals(-1, leetCodeEasy.strStr("leetcode", "leeto"))
+        assertEquals(0, leetCodeEasy.strStr("", ""))
+        assertEquals(0, leetCodeEasy.strStr("a", ""))
+    }
+
+    @Test
+    fun `test find the index of the first occurrence in a string without index() function`() {
+        assertEquals(0, leetCodeEasy.strStrWithoutBuildInFunction("sadbutsad", "sad"))
+        assertEquals(3, leetCodeEasy.strStrWithoutBuildInFunction("sadbutsad", "but"))
+        assertEquals(-1, leetCodeEasy.strStrWithoutBuildInFunction("leetcode", "leeto"))
+        assertEquals(0, leetCodeEasy.strStrWithoutBuildInFunction("", ""))
+        assertEquals(0, leetCodeEasy.strStrWithoutBuildInFunction("a", ""))
+        assertEquals(2, leetCodeEasy.strStrWithoutBuildInFunction("abc", "c"))
+    }
+
+    @Test
+    fun `search insert position`() {
+        assertEquals(2, leetCodeEasy.searchInsert(intArrayOf(1, 3, 5, 6), 5))
+        assertEquals(1, leetCodeEasy.searchInsert(intArrayOf(1, 3, 5, 6), 2))
+        assertEquals(4, leetCodeEasy.searchInsert(intArrayOf(1, 3, 5, 6), 7))
+    }
+
+    @Test
+    fun binarySearch() {
+        assertEquals(
+            4, leetCodeEasy.binarySearch(
+                intArrayOf(
+                    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+                    13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+                    25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36,
+                    37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48,
+                    49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
+                    61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72,
+                    73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84,
+                    85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96,
+                    97, 98, 99, 100
+                ), 5
+            )
+        )
+
+        assertEquals(
+            15, leetCodeEasy.binarySearch(
+                intArrayOf(
+                    1, 2, 3, 4, 5, 6, 7, 19, 20, 21, 22, 23, 24,
+                    25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36,
+                    37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48
+                ), 27
+            )
+        )
+        assertEquals(-1, leetCodeEasy.binarySearch(intArrayOf(1, 3, 5, 6), 2))
+        assertEquals(2, leetCodeEasy.searchInsert(intArrayOf(1, 3, 5, 6), 5))
+        assertEquals(0, leetCodeEasy.binarySearch(intArrayOf(1, 3, 5, 6), 1))
+    }
+
+    @Test
+    fun binarySearchRecursion() {
+        val nums = intArrayOf(
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+            13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+            25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36,
+            37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48,
+            49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
+            61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72,
+            73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84,
+            85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96,
+            97, 98, 99, 100
+        )
+        assertEquals(4, leetCodeEasy.binarySearchRecursion(nums, 5, start = 0, end = nums.lastIndex))
+
+        val nums1 = intArrayOf(
+            1, 2, 3, 4, 5, 6, 7, 19, 20, 21, 22, 23, 24,
+            25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36,
+            37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48
+        )
+        assertEquals(15, leetCodeEasy.binarySearchRecursion(nums1, 27, start = 0, end = nums1.lastIndex))
+        assertEquals(-1, leetCodeEasy.binarySearchRecursion(intArrayOf(1, 3, 5, 6), 2, start = 0, end = 3))
+        assertEquals(2, leetCodeEasy.binarySearchRecursion(intArrayOf(1, 3, 5, 6), 5, start = 0, end = 3))
+        assertEquals(0, leetCodeEasy.binarySearchRecursion(intArrayOf(1, 3, 5, 6), 1, start = 0, end = 3))
+    }
+
+    @Test
+    fun testLengthOfLastWord() {
+        assertEquals(5, leetCodeEasy.lengthOfLastWord("Hello World"))
+        assertEquals(4, leetCodeEasy.lengthOfLastWord("   fly me   to   the moon  "))
+        assertEquals(6, leetCodeEasy.lengthOfLastWord("fluffy is still joyboy"))
+        assertEquals(11, leetCodeEasy.lengthOfLastWord("he says abracadabra  "))
     }
 }
